2ba93bea9f17f223e82796a0f33832b4565a6969
felipeLx <felipealvesfinanceiro@gmail.com>
2021-11-03 19:29:25 +0200

    add project

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -7,7 +7,7 @@ import { PostProps } from '../../components/Post';
 import { useSession } from 'next-auth/client';
 import prisma from '../../lib/prisma';
 
-const URL = "https://vercel.com/felipelisboa/felipe-blog-nextjs-prisma-sql"
+const URL = "https:felipe-lisboa-blog-nextjs-prisma.vercel.app"
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
